3edf507aed47611aa9fe8edfd787d795a0a942c3
Yash Kumar <kryash90@gmail.com>
2023-10-12 17:43:03 +0530

    added unique email

--- src/subscription/schema/user.schema.ts
+++ src/subscription/schema/user.schema.ts
@@ -5,7 +5,7 @@ import { Document } from 'mongoose';
 export class User extends Document {
   
   
-  @Prop({ required: true })
+  @Prop({ required: true ,unique:true})
   email: string;
 
   @Prop()
